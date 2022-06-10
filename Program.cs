894cf1fff65f864c5b0d5dad4c2d281cd9247816
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-11 01:21:01 +0530

    [Tushar Vidwans] Validating First Name of the user using Regex

--- Program.cs
+++ Program.cs
@@ -9,7 +9,15 @@
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
+            UserRegistration userRegistration = new UserRegistration();
             Console.WriteLine("Welcome to the User Registraion problem");
+            Console.Write("Enter the First name: ");
+            string Firstname = Console.ReadLine();
+
+            if (userRegistration.validateFirstName(Firstname))
+                Console.WriteLine("Pattern Match");
+            else
+                Console.WriteLine("Pattern not Match");
         }
     }
 }
---
+++ UserRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration_Day19
+{
+    public class UserRegistration
+    {
+
+        public string regex_for_name = "^[A-Z]{1}[a-z]{2,10}$";
+
+        public bool validateFirstName(string fName)
+        {
+
+            return Regex.IsMatch(fName, regex_for_name);
+
+        }
+    }
+}
