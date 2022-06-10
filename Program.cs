15a9675874ae8c207e05ea82762081dc83c6f8c5
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-11 01:03:28 +0530

    [Tushar Vidwans] Adding Welcome message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UserRegistration_Day19
+{
+    class Program
+    {
+
+        /// <summary>
+        /// In this program we are validating user input with the regular expression.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registraion problem");
+        }
+    }
+}
